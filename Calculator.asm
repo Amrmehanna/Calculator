9233633eca60c0636ff70dc5ebe31d32b53df6c5
AhmedMahmoud125 <96206508+AhmedMahmoud125@users.noreply.github.com>
2021-12-27 01:28:02 +0200

    Update Calculator.asm

--- Calculator.asm
+++ Calculator.asm
@@ -1,4 +1,8 @@
-include\Irvine\Irvine32.inc
+include c:\masm32\include\windows.inc
+include c:\masm32\include\user32.inc
+include c:\masm32\include\kernel32.inc
+include c:\masm32\include\masm32.inc
+     
 includelib\masm32\lib\user32.lib
 includelib\Irvine\kernel32.lib
 includelib\Irvine\irvine32.lib
