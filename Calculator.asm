bea8f53268c7353a1f96ac763246cd9be11099d0
Mahmoudsayed77 <48159136+Mahmoudsayed77@users.noreply.github.com>
2021-12-27 16:54:56 +0200

    Update Calculator.asm

--- Calculator.asm
+++ Calculator.asm
@@ -8,10 +8,20 @@ includelib\Irvine\kernel32.lib
 includelib\Irvine\irvine32.lib
 
 
+.data
+
+startprogram               db " start the two operand assembly calculator ", 0
+endprogram                 db " we are happy to use our two operand calculator", 0
 
 
-startprogram          db " start the two operand assembly calculator ", 0
-endprogram            db " we are happy to use our two operand calculator", 0
+addition_sign              db '+', 0
+subtraction_sign           db '-', 0
+division_sign              db '/', 0
+muliplication_sign         db '*', 0
+space_sign                 db ' ', 0
+equal_sign                 db '=', 0
+parth_sign1                db '(', 0
+parh_sign2                 db ')', 0
 
 
 
