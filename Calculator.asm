7b7f44ee0165af79006a882ab61f67b8c801d609
Mahmoudsayed77 <48159136+Mahmoudsayed77@users.noreply.github.com>
2021-12-31 15:17:43 +0200

    Update Calculator.asm

--- Calculator.asm
+++ Calculator.asm
@@ -13,8 +13,14 @@ includelib\Irvine\irvine32.lib
 startprogram               DB " Welcome to the assembly calculator Porgram ", 0
 endprogram                 DB " we are happy to use our two operand calculator", 0
 
+first_operandstring        DB 16 dup (?)
+second_operandstring       DB 16 dup (?)
+first_operand_len          DB ?
+second_operand_len         DB ?
+
+
 first_operand              DD ?
-second_operand2            DD ?
+second_operand             DD ?
 operator                   DB ?
 result1                    DD ?
 
@@ -22,12 +28,15 @@ recall1                    DB "Enter the fisrt number, or (f/c) to exit", 0
 recall2                    DB "Enter the second number, or (f/c) to exit " 0
 recall3                    DB "Choose an operation (+, -, *, /), or (f/c) to exit ", 
 result2                    DB " result is: ", 0
+
+
 operand_msg                DB"<invalid number ,try another trial>",0
 operator_msg               DB"< invalid operator ,try another trial>",0
 overflow_msg0              DB"<result is large,   try another trial>",0
 overflow _msg1             DB"<first number is large,try another trial>",0
 overflow_msg2              DB"<secand number is large,try another trial>",0
 zero Div_msg               DB"<Divison by zero is invalid number>",0
+
  
 addition_sign              DB '+', 0
 subtraction_sign           DB '-', 0
